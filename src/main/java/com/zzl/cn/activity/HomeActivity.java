0bba854e90e935d497ddb75e4a3ca49e255aa7ba
zhangzl <948384063@qq.com>
2016-06-12 23:03:24 +0800

    Signed-off-by: zhangzl <948384063@qq.com>

--- src/main/java/com/zzl/cn/activity/HomeActivity.java
+++ src/main/java/com/zzl/cn/activity/HomeActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Toast;
 
+import com.zhy.http.okhttp.callback.StringCallback;
 import com.zzl.cn.R;
 import com.zzl.cn.adapter.HomeAdapter;
+import com.zzl.cn.utils.HttpApi;
+import com.zzl.cn.widget.MyToast;
+
+import okhttp3.Call;
 
 /**
  * Description:
@@ -38,7 +44,7 @@ public class HomeActivity extends BaseActivity {
             @Override
             public void onClickListener(View view, int position) {
                 // 点击事件处理
-                showToastMsg(data_[position]);
+                //showToastMsg(data_[position]);
                 dealClickListener(position);
             }
         });
@@ -47,8 +53,19 @@ public class HomeActivity extends BaseActivity {
     private void dealClickListener(int position) {
         switch (position) {
             default:
-                Intent intent = new Intent(HomeActivity.this, RecylerViewActivity.class);
-                startActivity(intent);
+//                Intent intent = new Intent(HomeActivity.this, RecylerViewActivity.class);
+//                startActivity(intent);
+                HttpApi.getApi(HomeActivity.this, "http://www.baidu.com", new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        MyToast.makeText(HomeActivity.this, "失敗："+id+e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        MyToast.makeText(HomeActivity.this, "成功："+response.substring(0, 10), Toast.LENGTH_LONG).show();
+                    }
+                });
                 break;
         }
     }
--- src/main/java/com/zzl/cn/utils/HttpApi.java
+++ src/main/java/com/zzl/cn/utils/HttpApi.java
@@ -1,9 +1,11 @@
 package com.zzl.cn.utils;
 
 import android.content.Context;
+import android.widget.Toast;
 
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
+import com.zzl.cn.widget.MyToast;
 
 import okhttp3.Call;
 
@@ -13,21 +15,11 @@ import okhttp3.Call;
  * date: 2016/6/12.
  */
 public class HttpApi {
-    public static void getApi(Context context, String url, StringCallback stringCallback) {
+    public static void getApi(final Context context, String url, StringCallback stringCallback) {
         OkHttpUtils.get()
+                .url(url)
                 .addParams("name", "zzl")
-        .addParams("pwd", "123")
         .build()
-        .execute(new StringCallback() {
-            @Override
-            public void onError(Call call, Exception e, int id) {
-
-            }
-
-            @Override
-            public void onResponse(String response, int id) {
-
-            }
-        });
+        .execute(stringCallback);
     }
 }
